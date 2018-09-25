3fa323ededea757198438c000cbd2003a4583e9a
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-09-25 09:54:43 -0400

    Tests: Use proper argument order for IS.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -5,16 +5,14 @@
 (cl:in-package #:anaphoric-variants_tests)
 
 (define-test "featured examples"
-  (is equal
+  (is equal '(c d)
       (anaphoric tail
         (when (member 'b '(a b c d))
-          (cdr tail)))
-      '(c d))
-  (is equal
+          (cdr tail))))
+  (is equal '(0 list a b c)
       (anaphoric (key :type type :index index)
         (etypecase '(a b c)
           (list (list* index type key))
-          (t (list index "Not a list." key))))
-      '(0 list a b c))
+          (t (list index "Not a list." key)))))
   ;;; TODO: Test other anaphoric variants, and custom anaphoric variant definition.
   )
